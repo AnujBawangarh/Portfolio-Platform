180d5f9f4814cbaece451981f0b12185c6139588
Vasishth <learnvasishth@gmail.com>
2026-02-09 16:04:04 +0530

    add connection between database and server

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-alert("Hare krsna");
+import app from "./app.js";
+import connectDB from "./src/config/connect.db.js";
+import dotenv from "dotenv";
+
+const port = process.env.PORT || 5000;
+dotenv.config({path : './.env'});
+
+connectDB()
+.then(()=>{
+    app.listen(port, () => {
+        console.log(`Server is running successfully http://localhost:${port}`);
+    })
+})
+.catch((error) => {
+    console.log(`MongoDB connection failed`, error);
+})
+
+
+
+
+
