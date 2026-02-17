20d0babbfe26ecbadf27eac429c08c7a2d452bd3
Anuj Bawangarh <anujbawangarh09@gmail.com>
2026-02-17 17:44:22 +0530

    All HTML Work Completed

---
+++ client/Portfolio_Page.js
@@ -0,0 +1,23 @@
+const github = document.getElementById('github');
+
+github.addEventListener('click', () => {
+    window.open('https://github.com/AnujBawangarh', "_blank");
+});
+
+const linkedin = document.getElementById('linkedin');
+
+linkedin.addEventListener('click', () => {
+    window.open('https://www.linkedin.com/in/anujbawangarh/', "_blank");
+});
+
+const instagram = document.getElementById('instagram');
+
+instagram.addEventListener('click', () => {
+    window.open('https://www.instagram.com/anujbawangarh/', "_blank");
+});
+
+const resumebtn = document.getElementById('resumebtn');
+
+resumebtn.addEventListener('click', () => {
+    window.open('https://drive.google.com/file/d/1n9l8sXo2mLh7a3e5j6k8l9m0n1o2p3q/view?usp=sharing', "_blank"); 
+});
