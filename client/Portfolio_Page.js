94d8932a06992aaec60060eae2b78d1afe938467
Anuj Bawangarh <anujbawangarh09@gmail.com>
2026-03-08 22:23:14 +0530

    08th March 2026 work committed

--- client/Portfolio_Page.js
+++ client/Portfolio_Page.js
@@ -15,3 +15,13 @@ const instagram = document.getElementById('instagram');
 instagram.addEventListener('click', () => {
     window.open('https://www.instagram.com/anujbawangarh/', "_blank");
 });
+
+window.addEventListener("scroll", function() {
+    const navbar = document.getElementById("heading");
+    // Check if the user has scrolled past a certain point (e.g., 50 pixels)
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
